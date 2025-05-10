fe09fa82c0be5ca41150420fc5952787f8edc4d3
Peter Mason <petermason@mac.home>
2025-05-10 21:28:18 +0100

    Restored working structure with blog and layout

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      appDir: true,
+    },
+    srcDir: 'src',
+  };
+  
+  export default nextConfig;
+  
