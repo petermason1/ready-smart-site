02de9ee7b7d244c6619e08052d9f82a3108d111b
Peter Mason <petermason@mac.home>
2025-05-11 09:59:17 +0100

    chore: clean up old CSS, add Smart Picks page and assets

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,23 +36,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        {/* Google Analytics */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-VHE1WE56M8"
-          strategy="afterInteractive"
-        />
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-VHE1WE56M8', {
-              page_path: window.location.pathname,
-            });
-          `}
-        </Script>
-      </head>
+     
       <body>
         <Navbar />
         {children}
